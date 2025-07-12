90e3ccb3a31f62967b2e922eb2d4256869bff7ef
Yusuf Shihata <yusufshihata2006@gmail.com>
2025-07-12 11:13:38 +0300

    start cpp infra

---
+++ src/cuda/core/device.h
@@ -0,0 +1,10 @@
+enum class DeviceType { CPU, CUDA };
+
+struct Device {
+    DeviceType type = DeviceType::CPU;
+    int index = 0;
+
+    bool operator==(const Device& other) const {
+        return type == other.type && index == other.index;
+    }
+};
